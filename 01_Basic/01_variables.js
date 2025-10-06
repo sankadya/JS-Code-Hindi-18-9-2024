59b9db9825e36a4ff2f4ca7a6f4870fefecd5233
sankadya <sanketpotle2429@gmail.com>
2025-10-06 21:27:02 +0530

    Updated homepage UI

--- 01_Basic/01_variables.js
+++ 01_Basic/01_variables.js
@@ -3,14 +3,14 @@ let accountEmail = "Demo@gmail.com"
 var accoundPassword = "98564"
 accountCity = "Mumbai"
 
-  accountEmail = "svp@gmail.com"
-  accountpassword = "pdlsf456"
-  accountCity = "Maharashtra"
+accountEmail = "svp@gmail.com"
+accountpassword = "pdlsf456"
+accountCity = "Maharashtra"
 
 /*
  prefer not to use var
  because of issue in block scope and functional scope 
 */
 
-  console.table([accoundId, accountEmail, accoundPassword, accountCity])
+console.table([accoundId, accountEmail, accoundPassword, accountCity])
 
